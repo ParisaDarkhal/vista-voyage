d684782531d0b408acd994ecbccbdc0e04cb48ce
ParisaDarkhal <p.darkhal@gmail.com>
2023-11-17 13:27:24 -0500

    popular dest card created

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "./main.css";
+import img from "../../Assets/img.jpg";
+import { HiOutlineLocationMarker } from "react-icons/hi";
+import { HiOutlineClipboardCheck } from "react-icons/hi";
 
 const Data = [
   {
@@ -81,7 +84,43 @@ const Main = () => {
         <h3 className="title">Most Visited Destinations</h3>
       </div>
 
-      <div className="secContent grid"></div>
+      <div className="secContent grid">
+        {Data.map(
+          ({ id, imgSrc, destTitle, location, grade, fees, description }) => {
+            return (
+              <div className="singleDestination" key={id}>
+                <div className="imageDiv">
+                  <img src={imgSrc} alt={destTitle} />
+                </div>
+                <div className="cardInfo">
+                  <h4 className="destTitle">{destTitle}</h4>
+                  <span className="continent flex">
+                    <HiOutlineLocationMarker className="icon" />
+                    <span className="name">{location}</span>
+                  </span>
+                  <div className="fees flex">
+                    <div className="grade">
+                      <span>
+                        {grade}
+                        <small> +1 </small>
+                      </span>
+                    </div>
+                    <div className="price">
+                      <h5>{fees}</h5>
+                    </div>
+                  </div>
+                  <div className="desc">
+                    <p>{description}</p>
+                  </div>
+                  <button className="btn flex">
+                    DETAILS <HiOutlineClipboardCheck className="icon" />
+                  </button>
+                </div>
+              </div>
+            );
+          }
+        )}
+      </div>
     </section>
   );
 };
